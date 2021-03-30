8afac91273c9521f4a2b4a501b6c4d20dc2f65c5
Oluwatobi Ishola <ishoshot@gmail.com>
2021-03-30 20:46:40 +0100

    Delete dist directory

--- dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._prod_ = void 0;
-exports._prod_ = process.env.NODE_ENV === "production";
-//# sourceMappingURL=constants.js.map
--- dist/utils/date.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getDate() {
-    var today = new Date();
-    return today;
-}
-exports.default = getDate;
-//# sourceMappingURL=date.js.map
